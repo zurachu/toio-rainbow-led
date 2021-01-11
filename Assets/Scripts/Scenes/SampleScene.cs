2875ed5614284300726359401c8a5df4492f7bb0
zurachu <zurachu@gmail.com>
2021-01-11 12:58:39 +0900

    first commit

---
+++ Assets/Scripts/Scenes/SampleScene.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using toio;
+
+public class SampleScene : MonoBehaviour
+{
+    [SerializeField] private Canvas canvas;
+    [SerializeField] private LicenseView licenseViewPrefab;
+
+    private CubeManager cubeManager;
+
+    private void Start()
+    {
+        cubeManager = new CubeManager();
+    }
+
+    public async void OnClickConnect()
+    {
+        var cube = await cubeManager.SingleConnect();
+        if (cube == null)
+        {
+            return;
+        }
+
+        var colors = new List<Color>();
+        colors.AddRange(Gradation(Color.red, Color.yellow, 4));
+        colors.AddRange(Gradation(Color.yellow, Color.green, 4));
+        colors.AddRange(Gradation(Color.green, Color.cyan, 4));
+        colors.AddRange(Gradation(Color.cyan, Color.blue, 4));
+        colors.AddRange(Gradation(Color.blue, Color.magenta, 4));
+        colors.AddRange(Gradation(Color.magenta, Color.red, 4));
+
+        cube.TurnOnLightWithScenario(0, colors.ConvertAll(_color => ToioLedUtility.LightOperationOf(_color, 100)).ToArray());
+    }
+
+    public void OnClickLicense()
+    {
+        Instantiate(licenseViewPrefab, canvas.transform);
+    }
+
+    private List<Color> Gradation(Color fromColor, Color toColor, int division)
+    {
+        var colors = new List<Color>();
+        for (var i = 0; i < division; i++)
+        {
+            colors.Add(Color.Lerp(fromColor, toColor, (float)(division - i) / division));
+        }
+
+        return colors;
+    }
+}
---
+++ Assets/Scripts/Views/LicenseView.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LicenseView : MonoBehaviour
+{
+    [SerializeField] private Text text;
+    [SerializeField] private TextAsset textAsset;
+
+    private void Start()
+    {
+        text.text = textAsset.text;
+    }
+
+    public void Close()
+    {
+        Destroy(gameObject);
+    }
+}
